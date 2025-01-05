5573ea09611d890e11114d347b794b138a63f23c
clairechang0609 <a115487@hotmail.com>
2025-01-06 00:36:47 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef, useEffect, SetStateAction, Dispatch, FC } from 'react';
+import './assets/app.css';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+const { VITE_API_BASE, VITE_API_PATH } = import.meta.env;
+
+interface LoginFormProps {
+  setIsLogin: Dispatch<SetStateAction<boolean>>;
+}
+
+// 登入表單
+const LoginForm: FC<LoginFormProps> = ({ setIsLogin }) => {
+  const [form, setForm] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleInput = (e: { target: { name: string; value: string } }) => {
+    const { name, value } = e.target;
+    setForm((pre) => ({
+      ...pre,
+      [name]: value
+    }))
+  }
+
+  const login = async (e: { preventDefault: () => void }) => {
+    try {
+      e.preventDefault();
+      const res = await axios.post(`${VITE_API_BASE}/admin/signin`, form);
+      const { token, expired } = res.data;
+      document.cookie = `andBloom=${token}; expires=${new Date(expired)};`;
+      setIsLogin(true);
+    } catch (err) {
+      console.log((err as Error).message);
+      setIsLogin(false);
+    }
+  }
+
+  return (
+    <form id="form" className="form" onSubmit={login}>
+      <input type="email" name="username" id="username" onChange={handleInput} />
+      <input type="password" name="password" id="password" onChange={handleInput} />
+      <button type="submit" className="btn btn-primary">Login</button>
+    </form>
+  )
+}
+
+const App = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+
+  // 檢查登入是否過期
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)andBloom\s*=\s*([^;]*).*$)|^.*$/,"$1",);
+        axios.defaults.headers.common.Authorization = token;
+        await axios.post(`${VITE_API_BASE}/api/user/check`);
+        setIsLogin(true);
+      } catch (err) {
+        setIsLogin(false);
+      }
+    })();
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        isLogin
+          ? '已登入'
+          : <LoginForm setIsLogin={setIsLogin} />
+      }
     </>
   )
 }
 
-export default App
+export default App;
