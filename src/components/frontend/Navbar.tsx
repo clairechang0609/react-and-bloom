2918f8203c6089cdd3e14a1d76926acb5ead92e9
clairechang0609 <a115487@hotmail.com>
2025-02-25 01:11:29 +0800

    feat: update frontend navbar

--- src/components/frontend/Navbar.tsx
+++ src/components/frontend/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef } from 'react';
+import { forwardRef, useEffect, useMemo, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router';
 import styled from 'styled-components';
@@ -44,9 +44,8 @@ const Heading = styled("h1")`
   margin: 0;
 `;
 
-const Navbar = () => {
+const Navbar = forwardRef<HTMLDivElement>((_props, ref) => {
   const dispatch = useAppDispatch();
-  const header = useRef<HTMLDivElement | null>(null);
   const { cart } = useSelector(cartData);
   const qty = useMemo(() => cart.reduce((acc, cur) => acc + cur.qty, 0), [cart]);
 
@@ -54,45 +53,36 @@ const Navbar = () => {
     dispatch(asyncGetCart({ isShowLoading: false }));
   }, [dispatch]);
 
-  useEffect(() => {
-    // if (header.current) {
-    //   new LocomotiveScroll({
-    //     el: header.current,
-    //     smooth: true,
-    //     lerp: .06,
-    //     multiplier: .5
-    //   });
-    // }
-  }, []);
-
   return (
-    <Nav ref={header} className="fixed-top py-3 px-5 d-flex align-items-center">
-      <NavLink to="/" className="d-flex align-items-center px-4">
-        <Heading>&<em>Bloom</em></Heading>
-      </NavLink>
-      <div className="d-flex align-items-center ms-auto">
-        <NavLink to="/products" className="px-4">
-          <Link>PLANTS・所有植栽</Link>
+    <header ref={ref} id="header">
+      <Nav className="py-3 px-5 fixed-top d-flex align-items-center">
+        <NavLink to="/" className="d-flex align-items-center px-4" data-scroll data-scroll-direction="horizontal" data-scroll-speed="-4" data-scroll-position="top">
+          <Heading>&<em>Bloom</em></Heading>
         </NavLink>
-        <NavLink to="/cart" className="px-4">
-          <Link className="position-relative">
-            CART・購物車
-            {
-              qty
-                ? (<span className="position-absolute top-25 start-100 translate-middle badge rounded-pill bg-success ms-1">
-                  {qty}
-                  <span className="visually-hidden">unread messages</span>
-                </span>)
-                : ''
-            }
-          </Link>
-        </NavLink>
-        <NavLink to="/admin" className="btn btn-sm btn-outline-primary rounded-pill px-4 mx-4 py-1">
-          後台
-        </NavLink>
-      </div>
-    </Nav>
+        <div className="d-flex align-items-center ms-auto" data-scroll data-scroll-direction="horizontal" data-scroll-speed="4" data-scroll-position="top">
+          <NavLink to="/products" className="px-4">
+            <Link>PLANTS・所有植栽</Link>
+          </NavLink>
+          <NavLink to="/cart" className="px-4">
+            <Link className="position-relative">
+              CART・購物車
+              {
+                qty
+                  ? (<span className="position-absolute top-25 start-100 translate-middle badge rounded-pill bg-success ms-1">
+                    {qty}
+                    <span className="visually-hidden">unread messages</span>
+                  </span>)
+                  : ''
+              }
+            </Link>
+          </NavLink>
+          <NavLink to="/admin" className="btn btn-sm btn-outline-primary rounded-pill px-4 mx-4 py-1">
+            後台
+          </NavLink>
+        </div>
+      </Nav>
+    </header>
   )
-};
+});
 
 export default Navbar;
